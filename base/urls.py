0ded69029d3c74b0844ef5cfd1cf856076e4ade4
Mohamad Massri <mouhamaddev04@gmail.com>
2023-05-25 21:12:53 +0300

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import MainView, CommentAnalyzer
-
+#urls
 urlpatterns = [
     path('api/<str:movie>/', MainView, name='main'),
     path('comment/<str:my_comment>/', CommentAnalyzer, name='comment'),
